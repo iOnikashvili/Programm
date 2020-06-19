40d2c8f27e88beb6600b3535c4fa892f2b790fb7
iOnikashvili <ionikashvili@bk.ru>
2020-06-19 21:31:57 +0500

    Экзамен2

---
+++ TaursAndCows/TaursAndCows/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace TaursAndCows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tryButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.newGameButton);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(243, 95);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Длина числа";
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(80, 45);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(155, 38);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "Начать";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(8, 45);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(66, 32);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.toolStripContainer1);
+            this.groupBox2.Controls.Add(this.tryButton);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(21, 144);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(243, 138);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Введите число";
+            this.groupBox2.Visible = false;
+            // 
+            // tryButton
+            // 
+            this.tryButton.Location = new System.Drawing.Point(8, 84);
+            this.tryButton.Name = "tryButton";
+            this.tryButton.Size = new System.Drawing.Size(227, 42);
+            this.tryButton.TabIndex = 1;
+            this.tryButton.Text = "Угадать";
+            this.tryButton.UseVisualStyleBackColor = true;
+            this.tryButton.Click += new System.EventHandler(this.tryButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.answerTextBox);
+            this.panel1.Location = new System.Drawing.Point(272, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(334, 245);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // answerTextBox
+            // 
+            this.answerTextBox.Location = new System.Drawing.Point(3, 3);
+            this.answerTextBox.Multiline = true;
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.ReadOnly = true;
+            this.answerTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.answerTextBox.Size = new System.Drawing.Size(330, 239);
+            this.answerTextBox.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(783, 16);
+            this.toolStripContainer1.Location = new System.Drawing.Point(-29, 208);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(783, 41);
+            this.toolStripContainer1.TabIndex = 4;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(550, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 299);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button tryButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox answerTextBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TaursAndCows/TaursAndCows/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TaursAndCows
+{
+    public partial class Form1 : Form
+    {
+        private Game _game;
+        private int _length;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsDigit(e.KeyChar) || e.KeyChar ==  (decimal) Keys.Back);
+        }
+
+
+
+        DateTime date1 = new DateTime(0, 0);
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            date1 = date1.AddSeconds(1);
+            label1.Text = date1.ToString("mm:ss");
+        }
+        public void newGameButton_Click(object sender, EventArgs e)
+        {
+            
+            if (timer1.Enabled == true)
+                timer1.Enabled = false;
+            else
+                timer1.Enabled = true;
+            if (newGameButton.Text == "Остановить") newGameButton.Text = "Начать";
+            else newGameButton.Text = "Остановить";
+            if (newGameButton.Text == "Остановить")
+            {
+                
+                _length = (int)numericUpDown1.Value;
+                answerTextBox.Text = "";
+                textBox1.MaxLength = _length;
+                _game = new Game(_length);
+                groupBox2.Visible = true;
+                panel1.Visible = true;
+            }
+            else
+            {
+                label1.Text = "00:00";
+                timer1.Stop();
+                date1 = new DateTime(0, 0);
+                _length = 0;
+                groupBox2.Visible = false;
+                panel1.Visible = false;
+            }
+        }
+
+        private void tryButton_Click(object sender, EventArgs e)
+        {
+            var number = textBox1.Text;
+            if (number.Length != _length)
+            {
+                MessageBox.Show("Длина должна быть равна "+ _length + "!");
+                return;
+            }
+            string result;
+            if (_game.Try(number, out result)) 
+                GameOver();
+            else 
+                answerTextBox.Text += string.Format("Ваш ответ: {0} \r\n{1}",number,result);
+        }
+
+        private void GameOver()
+        {
+            groupBox2.Visible = false;
+            MessageBox.Show("Вы угадали!" + "Затраченное время: " + label1.Text);
+            timer1.Stop();
+            groupBox2.Visible = false;
+            panel1.Visible = false;
+            newGameButton.Text = "Начать";
+        }
+    }
+}
